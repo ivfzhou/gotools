1510c91421cdf1e0466bbb5080773b52d5249e7e
ivfzhou <ivfzhou@126.com>
2023-10-19 22:03:50 +0800

    添加command函数

--- command.go
+++ command.go
@@ -17,24 +17,27 @@ import (
 	"io"
 	"os/exec"
 	"strings"
-	"sync/atomic"
+	"sync"
 )
 
 type Command struct {
 	cmd            *exec.Cmd
 	err            error
-	end            atomic.Int32
 	stdout, stderr bytes.Buffer
 	readCursor     int64
 	writeClose     io.WriteCloser
+	exit           chan struct{}
+	once           sync.Once
 }
 
+// Read 读取标准输出。
 func (c *Command) Read() []byte {
 	bs := c.stdout.Bytes()[c.readCursor:]
 	c.readCursor += int64(len(bs))
 	return bs
 }
 
+// Write 响应输入。
 func (c *Command) Write(input string) error {
 	if c.IsExit() {
 		return c.err
@@ -45,38 +48,58 @@ func (c *Command) Write(input string) error {
 	_, err := c.writeClose.Write([]byte(input))
 	if err != nil {
 		c.err = err
-		c.end.Store(1)
+		c.once.Do(func() { close(c.exit) })
 		c.cmd.Process.Kill()
 	}
 	return err
 }
 
+// IsExit 是否已运行完毕。
 func (c *Command) IsExit() bool {
-	return c.end.Load() > 0
+	select {
+	case <-c.exit:
+		return true
+	default:
+		return false
+	}
 }
 
+// Out 等待运行结束，返回运行结果
 func (c *Command) Out() (stdout, stderr []byte, err error) {
+	<-c.exit
 	return c.stdout.Bytes(), c.stderr.Bytes(), c.err
 }
 
 // RunCommand 交互式运行cmd命令。
 func RunCommand(cmd string) *Command {
 	command := exec.Command(cmd)
-	c := &Command{cmd: command}
+	c := &Command{cmd: command, exit: make(chan struct{})}
 	command.Stderr = &c.stderr
 	command.Stdout = &c.stdout
 	c.writeClose, c.err = command.StdinPipe()
 	if c.err != nil {
-		c.end.Store(1)
+		c.once.Do(func() { close(c.exit) })
 		return c
 	}
 	if c.err = command.Start(); c.err != nil {
-		c.end.Store(1)
+		c.once.Do(func() { close(c.exit) })
 		return c
 	}
 	go func() {
 		c.err = command.Wait()
-		c.end.Store(1)
+		c.once.Do(func() { close(c.exit) })
 	}()
 	return c
+}
+
+// RunCommandAndPrompt 运行命令并响应输入prompts。
+func RunCommandAndPrompt(cmd string, prompts ...string) (stdout, stderr []byte, err error) {
+	command := RunCommand(cmd)
+	for _, v := range prompts {
+		if err = command.Write(v); err != nil {
+			return
+		}
+	}
+	stdout, stderr, err = command.Out()
+	return
 }
